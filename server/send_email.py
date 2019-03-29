15c894ea5758d8878f5af9b1ea2251d924753382
ben <ben.gabay38@gmail.com>
2019-03-29 16:30:53 +0300

    when applied, the code organze to functions

--- server/send_email.py
+++ server/send_email.py
@@ -10,51 +10,31 @@ SMTP_SERVER_PORT = 587
 
 
 class Email:
-    """The class connects to a gmail account and send messages"""
-    def __init__(self, from_adrr, password):
-            self.from_adrr = from_adrr
-            self.password = password
-            self.file_name = ''
-            self.subject = ''
-            self.body = ''
-            self.to = ''
-            self.msg = ''
+    def __init__(self, from_address, password):
+        self.from_address = from_address
+        self.server = smtplib.SMTP(SMTP_SERVER_HOST, SMTP_SERVER_PORT)
+        self.server.starttls()
+        self.server.login(self.from_address, password)
 
-    def send_new_email(self, to, body, subject=None, file_name=''):
-        """The function gets the new email details, connect to the account with the from_adrr and password, and send
-         the email."""
-        self.subject = subject
-        self.body = body
-        self.file_name = file_name
-        self.to = to
+    def create_message(self, to, body, subject=None):
+        msg = EmailMessage()
+        msg['From'] = self.from_address
+        msg['To'] = to
+        msg.set_content(body, 'plain')
+        msg['Subject'] = subject
 
-        self.msg = EmailMessage()
-        self.msg['From'] = self.from_adrr
-        self.msg['To'] = self.to
+        return msg.as_string()
 
-        self.msg.set_content(self.body, 'plain')
+    def send_new_email(self, message):
         try:
-            self.add_file_to_message()
-        finally:
-            pass
-        try:
-            self.msg['Subject'] = self.subject
-        finally:
-            pass
-        try:
-            server = smtplib.SMTP(SMTP_SERVER_HOST, SMTP_SERVER_PORT)
-            server.starttls()
-            server.login(self.from_adrr, self.password)
-            text = self.msg.as_string()
-            server.sendmail(self.from_adrr, self.to, text)
-            server.quit()
+            self.server.sendmail(self.from_address, message)
             return "Success"
         except MessageError:
             return "Fail"
 
-    def add_file_to_message(self):
-        # The function attach file to the message
-        if os.path.exists(self.file_name):
-            attachment_file = open(self.file_name, "rb")
+    def attach_file(self, msg, file_path):
+        if os.path.exists(file_path):
+            attachment_file = open(file_path, "rb")
             image_data = attachment_file.read()
-            self.msg.add_attachment(image_data, maintype='image', subtype=imghdr.what(None, image_data))
+            msg.add_attachment(image_data, maintype='image', subtype=imghdr.what(None, image_data))
+

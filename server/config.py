4b74d3ab81fb21527e57173397585216f19016ec
ben <ben.gabay38@gmail.com>
2019-03-29 14:38:37 +0300

    added configuration

---
+++ server/config.py
@@ -0,0 +1,27 @@
+SERVER_IP = "0.0.0.0"
+SERVER_PORT = 6000
+NUM_CLIENTS = 100
+DATA_LENGTH = 1024
+
+ICON_PIC_NAME = "amd_icon.jpg"
+
+NORMAL_USER_PROCESS = "u0_a"
+BLACK_LIST_PROCESSES_NAMES = []
+WHITE_LIST_PROCESSES_USERS = ["root", "system", "shell", "wifi", "radio", "gps", "media", "bluetooth", "dhcp"]
+GOOGLE_PLAY_APP = "com.android.vending"
+
+APP_URL = "https://play.google.com/store/apps/details?id="
+AMAZON_APP = "com.amazon.venezia"
+SAMSUNG_APP_STORE_APP = "com.sec.android.app.samsungapps"
+SYSTEM_APP = "System App"
+FACEBOOK_APP = "com.facebook"
+UNKNOWN_APPS_BLACK_LIST_PERMISSIONS = ["ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION",
+                                       "ACCESS_LOCATION_EXTRA_COMMANDS", "ACCESS_NETWORK_STATE", "ACCESS_WIFI_STATE",
+                                       "CALL_PRIVILEGED", "READ_SMS", "RECEIVE_SMS", "SEND_SMS", "READ_CONTACTS"]
+
+FILE_DELIMITER = chr(28)
+CLIENT_DELIMITER = chr(30)
+SERVER_DELIMITER = chr(31) * 3
+
+EMAIL_USERNAME = "ben.gabay38@gmail.com"
+EMAIL_PASSWORD = "manchester2110#%"
